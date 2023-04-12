43255955579af0621740e6b6a433f684b82117fa
MineFuf <minefuf.fufik@gmail.com>
2023-04-12 18:47:14 +0200

    Add: option to display config path

--- tmaze/src/main.rs
+++ tmaze/src/main.rs
@@ -13,6 +13,8 @@ use game::{App, GameError};
 struct Args {
     #[clap(short, long, action, help = "Reset config to default and quit")]
     reset_config: bool,
+    #[clap(short, long, action, help = "Show config path and quit")]
+    show_config_path: bool,
 }
 
 fn main() -> Result<(), GameError> {
@@ -20,6 +22,15 @@ fn main() -> Result<(), GameError> {
 
     if _args.reset_config {
         settings::Settings::reset_config(settings::Settings::default_path());
+        return Ok(());
+    }
+
+    if _args.show_config_path {
+        if let Some(s) = settings::Settings::default_path().to_str() {
+            println!("{}", s);
+        } else {
+            println!("{:?}", settings::Settings::default_path());
+        }
         return Ok(());
     }
 
