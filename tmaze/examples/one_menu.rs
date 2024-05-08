97ca68c17750cb363027d72dc6d5f4f188cf2bbe
ur-fault <minefuf.fufik@gmail.com>
2024-05-08 13:27:52 +0200

    Add: edge case menu examples

---
+++ tmaze/examples/one_menu.rs
@@ -0,0 +1,39 @@
+use tmaze::{
+    app::{app::App, Activity, ActivityHandler},
+    ui::{menu, Popup},
+};
+
+fn main() {
+    let mut app = App::new(Activity::new_base(
+        "activity",
+        Box::new(MyActivity(
+            false,
+            Popup::new("Press any key to quit".to_string(), vec![]),
+        )),
+    ));
+
+    app.run();
+}
+
+struct MyActivity(bool, Popup);
+
+impl ActivityHandler for MyActivity {
+    fn update(&mut self, events: Vec<tmaze::app::Event>) -> Option<tmaze::app::Change> {
+        if !self.0 {
+            self.0 = true;
+
+            let menu_config = menu::MenuConfig::new("Menu", vec!["Option 1".to_string()])
+                .counted()
+                .default(1);
+
+            let menu = menu::Menu::new(menu_config).into_activity();
+            return Some(tmaze::app::Change::Push(menu));
+        }
+
+        self.1.update(events)
+    }
+
+    fn screen(&self) -> &dyn tmaze::ui::Screen {
+        &self.1
+    }
+}
