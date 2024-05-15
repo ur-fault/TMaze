81693408446b7270f9ec483c3f29cd07530b7304
ur-fault <minefuf.fufik@gmail.com>
2024-05-15 16:59:31 +0200

    Add: multisize strings, Fix: logging draw panic on small screen

--- tmaze/src/ui/helpers.rs
+++ tmaze/src/ui/helpers.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use cmaze::core::Dims;
+use unicode_width::UnicodeWidthStr;
 
 use crate::{helpers, renderer::helpers::term_size};
 
@@ -19,4 +20,47 @@ pub fn format_duration(dur: Duration) -> String {
         dur.as_secs() / 60,
         (dur.as_secs() % 60) as f32 + dur.subsec_millis() as f32 / 1000f32,
     )
+}
+
+pub fn smart_format_duration(dur: Duration, fract: bool) -> String {
+    if dur.as_secs() < 60 {
+        if fract {
+            format!(
+                "{:.1}s",
+                dur.as_secs() as f32 + dur.subsec_millis() as f32 / 1000f32
+            )
+        } else {
+            format!("{}s", dur.as_secs())
+        }
+    } else {
+        if fract {
+            format_duration(dur)
+        } else {
+            format!(
+                "{}m{}s",
+                dur.as_secs() / 60,
+                dur.as_secs() % 60 + dur.subsec_millis() as u64 / 1000
+            )
+        }
+    }
+}
+
+pub fn multisize_duration_format(dur: Duration, max_size: usize) -> String {
+    multisize_string(
+        vec![
+            smart_format_duration(dur, true),
+            smart_format_duration(dur, false),
+        ],
+        max_size,
+    )
+}
+
+pub fn multisize_string(strings: impl IntoIterator<Item = String>, max_size: usize) -> String {
+    let strings = &mut strings.into_iter();
+    let mut current = strings.next().unwrap();
+    while current.width() > max_size {
+        current = strings.next().unwrap();
+    }
+
+    current
 }
