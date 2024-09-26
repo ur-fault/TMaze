564bfd8f524cac96bc50b54ef766bd9187f7438b
ur-fault <minefuf.fufik@gmail.com>
2024-09-26 14:35:19 +0200

    Add: more styles

--- tmaze/src/ui/usecase/mod.rs
+++ tmaze/src/ui/usecase/mod.rs
@@ -1,1 +1,12 @@
+use dpad::dpad_theme_resolver;
+
+use crate::settings::theme::ThemeResolver;
+
 pub mod dpad;
+
+pub fn usedcase_ui_theme_resolver() -> ThemeResolver {
+    let mut resolver = ThemeResolver::new();
+    resolver.extend(dpad_theme_resolver());
+
+    resolver
+}
