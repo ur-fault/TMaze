d436eab452c57caa494be34424a2e0a9b71f7d42
ur-fault <minefuf.fufik@gmail.com>
2025-08-17 10:05:00 +0200

    Fix: multicell fill, ::clear_space()

---
+++ tmaze/examples/multicell_madness.rs
@@ -0,0 +1,31 @@
+use std::io::stdout;
+
+use cmaze::dims::Dims;
+use tmaze::{
+    renderer::{CellContent, GBuffer},
+    settings::theme::{Color, NamedColor, Style},
+};
+
+fn main() {
+    let style = Style::default();
+    let red = Style {
+        fg: Some(Color::Named(NamedColor::Black)),
+        bg: Some(Color::Named(NamedColor::Red)),
+        ..Default::default()
+    };
+    let mut buf = GBuffer::new(Dims(19, 8));
+    buf.mut_view().border(style).inside(|f| {
+        f.fill(CellContent::styled(
+            ' ',
+            Style::bg(Color::Named(NamedColor::DarkYellow)),
+        ))
+        .fill(CellContent::styled('あ', red))
+        .centered(Dims(6, 2), |f| {
+            f.fill(CellContent::styled(
+                '$',
+                Style::bg(Color::Named(NamedColor::DarkYellow)),
+            ));
+        });
+    });
+    buf.write(&mut stdout()).unwrap();
+}
